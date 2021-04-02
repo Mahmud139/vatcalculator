006b2cbfd35b0bd0da6d61b79408bdb9c145487a
Mahmud139 <52196939+Mahmud139@users.noreply.github.com>
2021-04-03 01:07:35 +0600

    bug fixed

--- vatcalculator.go
+++ vatcalculator.go
@@ -3,12 +3,12 @@ package vatcalculator
 import (
 	"math"
 )
-func InclusiveTax(amount float64, tax float64) float64{
+func ExclusiveTax(amount float64, tax float64) float64{
 	calculation:=amount*(tax/100)
 	return math.Round(calculation)
 }
 
-func ExclusiveTax(amount float64, tax float64) float64{
+func InclusiveTax(amount float64, tax float64) float64{
 	calculation:=amount+amount*(tax/100)
 	totalPriceAfterTax:=amount*(amount/calculation)
 	totalTax:=amount-totalPriceAfterTax
